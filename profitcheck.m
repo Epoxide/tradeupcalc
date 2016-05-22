bd8b7041940b8347630a8a4e85d04ee24a85b06c
Epoxide <Epoxide@users.noreply.github.com>
2016-05-22 21:59:23 +0200

    Update profitcheck.m

--- profitcheck.m
+++ profitcheck.m
@@ -10,22 +10,22 @@ skindata = namewear2data(skinname,wear);
 [~, remain] = strtok(remain(2:end), char(10));
 [~, remain] = strtok(remain(2:end), char(10));
 [~, remain] = strtok(remain(2:end), char(10));
-[marketprize, remain] = strtok(remain(2:end), char(10));
-opprize = strtok(remain(2:end), char(10));
+[marketprice, remain] = strtok(remain(2:end), char(10));
+opprice = strtok(remain(2:end), char(10));
 %Trade up skins
-[skinspostdata,prizes,outcomenumbers] = tradeuplogic(skindata,avgfloat);
+[skinspostdata,prices,outcomenumbers] = tradeuplogic(skindata,avgfloat);
 %Cost check
-skinsmarketcost = 10*str2double(marketprize);
-skinsopcost = 10*str2double(opprize);
+skinsmarketcost = 10*str2double(marketprice);
+skinsopcost = 10*str2double(opprice);
 %Profit
 n = 1;
-profitmarketprize = zeros(outcomenumbers,1);
-profitopprize = zeros(outcomenumbers,1);
+profitmarketprice = zeros(outcomenumbers,1);
+profitopprice = zeros(outcomenumbers,1);
 for i = 1:outcomenumbers
-    earnedmarketprize = str2double(prizes{n+6});
-    profitmarketprize(i) = earnedmarketprize-skinsmarketcost;
-    earnedopprize = str2double(prizes{n+7});
-    profitopprize(i) = earnedopprize-skinsopcost;
+    earnedmarketprice = str2double(prices{n+6});
+    profitmarketprice(i) = earnedmarketprice-skinsmarketcost;
+    earnedopprice = str2double(prices{n+7});
+    profitopprice(i) = earnedopprice-skinsopcost;
     n = n + 9;
 end
 percentage = num2str(100/outcomenumbers);
@@ -35,6 +35,6 @@ disp(['Used skin(s): ' skinname ' ' wear]);
 disp(['Average float: ' avgfloat char(10)]);
 disp(['Possible outcomes: ' char(10)]);
 disp(skinspostdata);
-disp([char(10) 'The trade-up will cost you $' num2str(skinsmarketcost) ' on the Steam market and $' num2str(skinsopcost) ' on OPskins.' char(10)]);
-disp(['You have a ' percentage '% to earn/lose $' num2str(profitmarketprize(1)) ' and a ' percentage '% to earn/lose $' num2str(profitmarketprize(2)) ' on the Steam market.' char(10)]);
-disp(['You have a ' percentage '% to earn/lose $' num2str(profitopprize(1)) ' and a ' percentage '% to earn/lose $' num2str(profitopprize(2)) ' on OPskins.' char(10)]);
+disp([char(10) 'The trade-up will cost you €' num2str(skinsmarketcost) ' on the Steam market and €' num2str(skinsopcost) ' on OPskins.' char(10)]);
+disp(['You have a ' percentage '% to earn/lose €' num2str(profitmarketprice(1)) ' and a ' percentage '% to earn/lose €' num2str(profitmarketprice(2)) ' on the Steam market.' char(10)]);
+disp(['You have a ' percentage '% to earn/lose €' num2str(profitopprice(1)) ' and a ' percentage '% to earn/lose €' num2str(profitopprice(2)) ' on OPskins.' char(10)]);
